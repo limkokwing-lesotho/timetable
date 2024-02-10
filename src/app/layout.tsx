ad79eb99c4c1932acaff324fa62b712eb8b7bcab
Ntholi Nkhatho <ntholi@live.com>
2024-02-10 16:57:15 +0200

    create common layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
@@ -16,7 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className='mt-10 text-white'>
+          <Link href='/' className='flex justify-center'>
+            <Image alt='Logo' src='/logo.png' width={280} height={125} />
+          </Link>
+          <div className='mt-20' />
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import Image from 'next/image';
 import { classes } from './modal';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
+    <section className='flex flex-col gap-3 items-center'>
       {classes.map((it) => (
         <Link key={it.code} href={`/${it.code}`}>
           {it.name}
         </Link>
       ))}
-    </main>
+    </section>
   );
 }
