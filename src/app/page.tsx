257fde615b405021457281cbb60249e0e25e0559
Ntholi Nkhatho <ntholi@live.com>
2024-02-10 19:49:25 +0200

    Refactor class links in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <section className='flex flex-col gap-5 items-center'>
+    <section className='space-y-5'>
       {classes.map((it) => (
-        <Link key={it.code} href={`/${it.code}`}>
+        <Link key={it.code} href={`/${it.code}`} className='text-center block'>
           {it.name}
         </Link>
       ))}
