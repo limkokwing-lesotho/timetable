8c8e4d5c7ac2483116a040258fe1e2415c89d2c3
Ntholi Nkhatho <ntholi@live.com>
2024-02-11 06:10:01 +0200

    Refactor page component to display program name and class codes

--- src/app/[program]/page.tsx
+++ src/app/[program]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Timetable } from '../../lib/modal';
+import { Timetable, classes } from '../../lib/modal';
 import { getData } from '@/lib/data';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
@@ -13,26 +13,33 @@ type Params = {
 export default async function page({ params }: Params) {
   const { program } = params;
   const data = await getData();
-  const classes = Object.keys(data).filter((it) => it.startsWith(program));
+  const classCodes = Object.keys(data).filter((it) => it.startsWith(program));
+
+  const programName = classes.filter((it) => it.code === program)[0].name;
 
   return (
-    <div className='flex flex-col items-stretch sm:items-center gap-3'>
-      {classes.map((it) => (
-        <Button
-          key={it}
-          asChild
-          className='py-8 sm:min-w-[30vw]'
-          variant={'outline'}
-        >
-          <Link
-            href={`/${program}/timetables/${it}`}
+    <>
+      <h1 className='text-lg mt-5 mb-10 text-center text-zinc-200'>
+        {programName}
+      </h1>
+      <div className='flex flex-col items-stretch sm:items-center gap-3'>
+        {classCodes.map((it) => (
+          <Button
             key={it}
-            className='text-center block'
+            asChild
+            className='py-8 sm:min-w-[30vw]'
+            variant={'outline'}
           >
-            {it}
-          </Link>
-        </Button>
-      ))}
-    </div>
+            <Link
+              href={`/${program}/timetables/${it}`}
+              key={it}
+              className='text-center block'
+            >
+              {it}
+            </Link>
+          </Button>
+        ))}
+      </div>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
 import { classes } from '../lib/modal';
 import Link from 'next/link';
-import { Button, buttonVariants } from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <section className='flex flex-col items-stretch sm:items-center gap-3'>
-      {classes.map((it) => (
-        <Button
-          key={it.code}
-          asChild
-          className='py-8 sm:min-w-[30vw]'
-          variant={'outline'}
-        >
-          <Link key={it.code} href={`/${it.code}`}>
-            {it.name}
-          </Link>
-        </Button>
-      ))}
-    </section>
+    <>
+      <section className='flex flex-col items-stretch sm:items-center gap-3'>
+        {classes.map((it) => (
+          <Button
+            key={it.code}
+            asChild
+            className='py-8 sm:min-w-[30vw]'
+            variant={'outline'}
+          >
+            <Link key={it.code} href={`/${it.code}`}>
+              {it.name}
+            </Link>
+          </Button>
+        ))}
+      </section>
+    </>
   );
 }
